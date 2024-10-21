4c39244707bf9f5ed2898d955573ce96b33126fc
tommytan <sage.ts920126@gmail.com>
2024-10-21 15:10:37 +0800

    add begin transaction method to wallet service to ensure each withdraw, deposit and transfer will execute both wallet update and transaction record

--- tests/mocks/transaction/mock_transaction_service.go
+++ tests/mocks/transaction/mock_transaction_service.go
@@ -2,6 +2,7 @@ package mock_transaction
 
 import (
 	"centralized-wallet/internal/models"
+	"database/sql"
 
 	"github.com/stretchr/testify/mock"
 )
@@ -11,14 +12,14 @@ type MockTransactionService struct {
 }
 
 // RecordTransaction mocks the RecordTransaction function
-func (m *MockTransactionService) RecordTransaction(fromWalletNumber, toWalletNumber *string, transactionType string, amount float64) error {
-	args := m.Called(fromWalletNumber, toWalletNumber, transactionType, amount)
+func (m *MockTransactionService) RecordTransaction(tx *sql.Tx, fromWalletNumber, toWalletNumber *string, transactionType string, amount float64) error {
+	args := m.Called(tx, fromWalletNumber, toWalletNumber, transactionType, amount)
 	return args.Error(0)
 }
 
 // GetTransactionHistory mocks the GetTransactionHistory function
 func (m *MockTransactionService) GetTransactionHistory(walletNumber string, orderBy string, limit, offset int) ([]models.TransactionWithEmails, error) {
-	args := m.Called(walletNumber, orderBy, limit)
+	args := m.Called(walletNumber, orderBy, limit, offset)
 	if args.Get(0) == nil {
 		return nil, args.Error(1)
 	}
--- tests/mocks/wallet/mock_wallet_repository.go
+++ tests/mocks/wallet/mock_wallet_repository.go
@@ -2,6 +2,7 @@ package mock_wallet
 
 import (
 	"centralized-wallet/internal/models"
+	"database/sql"
 
 	"github.com/stretchr/testify/mock"
 )
@@ -17,6 +18,15 @@ type MockWalletRepository struct {
 func (m *MockWalletRepository) CreateWallet(wallet *models.Wallet) error {
 	args := m.Called(wallet)
 	return args.Error(0)
+}
+
+// mock begin transaction
+func (m *MockWalletRepository) Begin() (*sql.Tx, error) {
+	args := m.Called()
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*sql.Tx), args.Error(1)
 }
 
 // IsWalletNumberExists mocks the IsWalletNumberExists function
@@ -41,8 +51,8 @@ func (m *MockWalletRepository) GetWalletByUserID(userID int) (*models.Wallet, er
 }
 
 // Deposit mocks the Deposit function
-func (m *MockWalletRepository) Deposit(userID int, amount float64) (*models.Wallet, error) {
-	args := m.Called(userID, amount)
+func (m *MockWalletRepository) Deposit(tx *sql.Tx, userID int, amount float64) (*models.Wallet, error) {
+	args := m.Called(tx, userID, amount)
 	if args.Get(0) == nil {
 		return nil, args.Error(1)
 	}
@@ -50,17 +60,8 @@ func (m *MockWalletRepository) Deposit(userID int, amount float64) (*models.Wall
 }
 
 // Withdraw mocks the Withdraw function
-func (m *MockWalletRepository) Withdraw(userID int, amount float64) (*models.Wallet, error) {
-	args := m.Called(userID, amount)
-	if args.Get(0) == nil {
-		return nil, args.Error(1)
-	}
-	return args.Get(0).(*models.Wallet), args.Error(1)
-}
-
-// Transfer mocks the Transfer function
-func (m *MockWalletRepository) Transfer(fromUserID int, toWalletNumber string, amount float64) (*models.Wallet, error) {
-	args := m.Called(fromUserID, toWalletNumber, amount)
+func (m *MockWalletRepository) Withdraw(tx *sql.Tx, userID int, amount float64) (*models.Wallet, error) {
+	args := m.Called(tx, userID, amount)
 	if args.Get(0) == nil {
 		return nil, args.Error(1)
 	}
